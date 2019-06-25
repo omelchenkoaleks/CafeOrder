b08662eb6e251294166999443cd40649942c5dc8
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-06-25 13:30:19 +0300

    получаем через интент имя и подставляем его в TextView ...

--- app/src/main/java/com/omelchenkoaleks/cafeorder/CreateOrderActivity.java
+++ app/src/main/java/com/omelchenkoaleks/cafeorder/CreateOrderActivity.java
@@ -2,6 +2,7 @@ package com.omelchenkoaleks.cafeorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -17,10 +18,27 @@ public class CreateOrderActivity extends AppCompatActivity {
     private Spinner mTeaSpinner;
     private Spinner mCoffeeSpinner;
 
+    // хранит название выбранного напитка
+    private String mDrink;
+
+    private String mName;
+    private String mPassword;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_order);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("name") && intent.hasExtra("password")) {
+            mName = intent.getStringExtra("name");
+            mPassword = intent.getStringExtra("password");
+        } else {
+            mName = getString(R.string.default_name);
+            mPassword = getString(R.string.default_password);
+        }
 
         mHelloTextView = findViewById(R.id.hello_text_view);
         mAdditionsTextView = findViewById(R.id.additions_text_view);
@@ -29,6 +47,12 @@ public class CreateOrderActivity extends AppCompatActivity {
         mLemonCheckBox = findViewById(R.id.lemon_check_box);
         mTeaSpinner = findViewById(R.id.tea_spinner);
         mCoffeeSpinner = findViewById(R.id.coffee_spinner);
+
+        mDrink = getString(R.string.tea);
+
+        // %s - это место для заполнения (имени)
+        String hello = String.format(getString(R.string.hello_user_text_view), mName);
+        mHelloTextView.setText(hello);
     }
 
     public void onClickChangeDrink(View view) {
